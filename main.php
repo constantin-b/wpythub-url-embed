dd9a16a614449336f89d670585e627bf40416ab3
Constantin <constantin.b@gmail.com>
2022-03-23 11:30:44 +0200

    Updated version to 1.0.1

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wpythub.com
  * Description: Import YouTube videos into post and embed them by directly placing the video URL into the post content.
  * Author: CodeFlavors
- * Version: 1.0
+ * Version: 1.0.1
  * Author URI: https://wpythub.com
  */
 
